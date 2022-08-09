7e087ea5e12592c347d708ee0397ec771fad5750
Marving Moreton <moreton.marving@gmail.com>
2022-08-09 15:54:55 -0400

    MVC done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,54 @@
 class ArticlesController < ApplicationController
 
+  # Read all
   def index
     @articles = Article.all
+  end
+
+  # read one
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # CREATE
+
+  def new
+    # Save the instance of Article in a instance variable named article
+    @article = Article.new
+  end
+
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    # Redirect coz no view for create, the post shld redirect to the created article individual page (show)
+    redirect_to article_path(@article)
+  end
+
+  # EDIT
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    # redirect
+    redirect_to article_path(@article)
+  end
+
+  # DELETE
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # redirect
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 
 end
